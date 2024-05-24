e8b6514c178ed8eed7ed7971455069108d6570c7
Qing浅 <shengtong9828@163.com>
2024-05-24 22:26:04 +0800

    chore: :hammer: 菜单增加登陆

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -49,82 +49,10 @@
             <div>{{ weather.condition.aqi["pm2.5"] }}</div>
             <div>{{ weather.condition.aqi.so2 }}</div>
             <div>{{ weather.condition.aqi.update_time }}</div>
-
-            <el-statistic
-              v-for="item in statisticData"
-              :key="item.key"
-              :value="item.value"
-            >
-              <template #title>
-                <div class="flex items-center">
-                  <svg-icon :icon-class="item.iconClass" size="20px" />
-                  <span class="text-[16px] ml-1">{{ item.title }}</span>
-                </div>
-              </template>
-              <template v-if="item.suffix" #suffix>/100</template>
-            </el-statistic>
           </div>
         </el-col>
       </el-row>
     </el-card>
-
-    <!-- 数据卡片 -->
-    <el-row v-if="false" :gutter="10" class="mt-3">
-      <el-col
-        :xs="24"
-        :sm="12"
-        :lg="6"
-        v-for="(item, index) in cardData"
-        :key="index"
-      >
-        <el-card shadow="never">
-          <template #header>
-            <div class="flex items-center justify-between">
-              <span class="text-[var(--el-text-color-secondary)]">{{
-                item.title
-              }}</span>
-              <el-tag :type="item.tagType">
-                {{ item.tagText }}
-              </el-tag>
-            </div>
-          </template>
-
-          <div class="flex items-center justify-between mt-5">
-            <div class="text-lg text-right">
-              {{ Math.round(item.count) }}
-            </div>
-            <svg-icon :icon-class="item.iconClass" size="2em" />
-          </div>
-
-          <div
-            class="flex items-center justify-between mt-5 text-sm text-[var(--el-text-color-secondary)]"
-          >
-            <span> {{ item.dataDesc }} </span>
-            <span> {{ Math.round(item.count * 15) }} </span>
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
-
-    <!-- Echarts 图表 -->
-    <el-row v-if="false" :gutter="10" class="mt-3">
-      <el-col
-        :xs="24"
-        :sm="12"
-        :lg="8"
-        class="mb-2"
-        v-for="item in chartData"
-        :key="item"
-      >
-        <component
-          :is="chartComponent(item)"
-          :id="item"
-          height="400px"
-          width="100%"
-          class="bg-[var(--el-bg-color-overlay)]"
-        />
-      </el-col>
-    </el-row>
   </div>
 </template>
 
@@ -176,93 +104,6 @@ const greetings = computed(() => {
     return "偷偷向银河要了一把碎星，只等你闭上眼睛撒入你的梦中，晚安🌛！";
   }
 });
-
-const duration = 5000;
-
-// 销售额
-const amount = ref(0);
-const amountOutput = useTransition(amount, {
-  duration: duration,
-  transition: TransitionPresets.easeOutExpo,
-});
-amount.value = 2000;
-
-// 访客数
-const visitCount = ref(0);
-const visitCountOutput = useTransition(visitCount, {
-  duration: duration,
-  transition: TransitionPresets.easeOutExpo,
-});
-visitCount.value = 2000;
-
-// IP数
-const dauCount = ref(0);
-const dauCountOutput = useTransition(dauCount, {
-  duration: duration,
-  transition: TransitionPresets.easeOutExpo,
-});
-dauCount.value = 2000;
-
-// 订单量
-const orderCount = ref(0);
-const orderCountOutput = useTransition(orderCount, {
-  duration: duration,
-  transition: TransitionPresets.easeOutExpo,
-});
-orderCount.value = 2000;
-
-interface CardProp {
-  title: string;
-  tagType: EpPropMergeType<
-    StringConstructor,
-    "primary" | "success" | "info" | "warning" | "danger",
-    unknown
-  >;
-  tagText: string;
-  count: any;
-  dataDesc: string;
-  iconClass: string;
-}
-// 卡片数量
-const cardData = ref<CardProp[]>([
-  {
-    title: "访客数",
-    tagType: "success",
-    tagText: "日",
-    count: visitCountOutput,
-    dataDesc: "总访客数",
-    iconClass: "visit",
-  },
-  {
-    title: "IP数",
-    tagType: "success",
-    tagText: "日",
-    count: dauCountOutput,
-    dataDesc: "总IP数",
-    iconClass: "ip",
-  },
-  {
-    title: "销售额",
-    tagType: "primary",
-    tagText: "月",
-    count: amountOutput,
-    dataDesc: "总IP数",
-    iconClass: "money",
-  },
-  {
-    title: "订单量",
-    tagType: "danger",
-    tagText: "季",
-    count: orderCountOutput,
-    dataDesc: "总订单量",
-    iconClass: "order",
-  },
-]);
-// 图表数据
-const chartData = ref(["BarChart", "PieChart", "RadarChart"]);
-const chartComponent = (item: string) => {
-  return defineAsyncComponent(() => import(`./components/${item}.vue`));
-};
 </script>
 
 <style lang="scss" scoped>
